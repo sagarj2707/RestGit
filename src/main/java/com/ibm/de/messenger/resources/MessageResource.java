e4e3b4f732780495f1f6ad6dd8eaf06001f6d8ef
jadhavsagar <jadhav.tukl@gmail.com>
2016-04-18 13:42:17 +0200

    Response Format JSON added

--- src/main/java/com/ibm/de/messenger/resources/MessageResource.java
+++ src/main/java/com/ibm/de/messenger/resources/MessageResource.java
@@ -4,19 +4,28 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.ibm.de.messenger.model.Message;
 import com.ibm.de.messenger.service.MessageService;
 
+
 @Path("/messages")
 public class MessageResource {
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
+	}
+
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
 	}
 }
